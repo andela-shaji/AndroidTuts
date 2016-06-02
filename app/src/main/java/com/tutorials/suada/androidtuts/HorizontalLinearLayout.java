9dd27c26277c041513cd3247226c26292c4d7e9e
miss-android-java <suadahaji@gmail.com>
2016-06-02 16:53:43 +0300

    Add button for RelativeLayout Activity in the HLinearLayout Activity

--- app/src/main/java/com/tutorials/suada/androidtuts/HorizontalLinearLayout.java
+++ app/src/main/java/com/tutorials/suada/androidtuts/HorizontalLinearLayout.java
@@ -1,7 +1,9 @@
 package com.tutorials.suada.androidtuts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HorizontalLinearLayout extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class HorizontalLinearLayout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horizontal_linear_layout);
+    }
+    public void relativeLayout(View view) {
+        Intent intent = new Intent(this, RelativeLayout.class);
+        startActivity(intent);
     }
 }
